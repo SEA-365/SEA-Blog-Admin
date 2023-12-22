02224da4ab66c0b4db78bdd4044e56ef2a8e7238
SEA-365 <whm360735@163.com>
2023-12-22 18:06:31 +0800

    :blush:20231222-个人介绍页面

--- src/views/user/introduction.vue
+++ src/views/user/introduction.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    个人介绍
-  </div>
+  <el-card class="box-card1">
+
+
+    <el-card class="box-card" >
+      <div slot="header" class="clearfix">
+        <span>个人简介</span>
+      </div>
+      <div class="text item">
+        趁年轻，多折腾。
+      </div>
+    </el-card>
+
+  </el-card>
 </template>
 
 <script>
@@ -14,5 +24,33 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+.text {
+  font-size: 14px;
+}
 
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 70%;
+  float: left;
+  margin: 2% 6%;
+}
+.box-card1 {
+  width: 98%;
+  height: 90vh;
+  margin: 1%;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+.clearfix span {
+  font-weight: 600;
+}
 </style>
